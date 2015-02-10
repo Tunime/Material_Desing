1f31ea95538b3c272af2aa615de1f222126c292f
juliocsar13 <jc_csar_15@hotmail.com>
2015-02-09 20:28:00 -0500

    desing resoluciones

--- app/src/main/java/com/example/lenovo/material_desing/MainActivity.java
+++ app/src/main/java/com/example/lenovo/material_desing/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 
 
 public class MainActivity extends Activity {
@@ -11,8 +12,11 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_CONTENT_TRANSITIONS);
         setContentView(R.layout.activity_main);
     }
+
+
 
 
     @Override
